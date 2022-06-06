89c60473aa307c6813be8bd8fa33b08f1a94ba2f
leechanhoe <54972898+leechanhoe@users.noreply.github.com>
2022-06-06 20:32:12 +0900

    스프링 입문 강의 14강까지 수강

---
+++ hello-spring/src/main/java/dodo/hellospring/domain/Member.java
@@ -0,0 +1,22 @@
+package dodo.hellospring.domain;
+
+public class Member {
+    private Long id; // 고객 id가아니라 시스템이 정하는 id
+    private String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
