32444d5075191b9b80e841ae259d3813cda977b4
leechanhoe <54972898+leechanhoe@users.noreply.github.com>
2022-06-11 21:48:09 +0900

    study pure JDBC 22.6.11

--- hello-spring/src/main/java/dodo/hellospring/SpringConfig.java
+++ hello-spring/src/main/java/dodo/hellospring/SpringConfig.java
@@ -1,13 +1,25 @@
 package dodo.hellospring;
 
+import dodo.hellospring.repository.JdbcMemberRepository;
 import dodo.hellospring.repository.MemberRepository;
 import dodo.hellospring.repository.MemoryMemberRepository;
 import dodo.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.sql.init.DatabaseInitializationSettings;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+
+    private DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     @Bean // 스프링빈에 등록하라는뜻
     public MemberService memberService(){
@@ -16,6 +28,6 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 }
